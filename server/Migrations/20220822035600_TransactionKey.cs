1d436793c6460f7371e703b48ceedaa6595b17df
isabela <isabelavdmatos@gmail.com>
2022-08-22 00:56:31 -0300

    add migration for new transaction key

---
+++ server/Migrations/20220822035600_TransactionKey.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cube_api.Migrations
+{
+    public partial class TransactionKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_TypeId",
+                table: "Transactions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TypeId_Date_ProductDescription_Value_SellerName",
+                table: "Transactions",
+                columns: new[] { "TypeId", "Date", "ProductDescription", "Value", "SellerName" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_TypeId_Date_ProductDescription_Value_SellerName",
+                table: "Transactions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TypeId",
+                table: "Transactions",
+                column: "TypeId");
+        }
+    }
+}
