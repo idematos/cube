97b31699e5cc3ce5f8075b0399a1dce11df2d64d
isabela <isabelavdmatos@gmail.com>
2022-08-22 23:32:34 -0300

    add filter by seller

--- client/src/views/transactions.tsx
+++ client/src/views/transactions.tsx
@@ -1,45 +1,65 @@
 import { ReactElement, useEffect, useState } from "react"
 
 import axios, { AxiosResponse } from "axios"
+import get from "lodash/get"
 import { TbUpload } from "react-icons/tb"
 
 import Balance from "../components/balance"
 import Button from "../components/button"
 import PageLayout from "../components/pageLayout"
 import Select from "../components/select"
-import Table, { TableColumns } from "../components/table/table"
+import Table from "../components/table/table"
 import UploadModal from "../components/uploadModal"
 
 function fetchTransactions(): Promise<AxiosResponse> {
   return axios.get("Transaction")
 }
 
-const transactionColumns: TableColumns[] = [
-  {
-    path: "date",
-    label: "Date",
-  },
-  {
-    path: "type.description",
-    label: "Type",
-  },
-  {
-    path: "sellerName",
-    label: "Seller",
-  },
-  {
-    path: "productDescription",
-    label: "Product",
-  },
-  {
-    path: "value",
-    label: "Value",
-  },
-]
-
 function Transactions(): ReactElement {
+  const allSellers = "All sellers"
+  const expenseTypeId = 3
+
   const [isUploadModalOpen, setIsUploadModalOpen] = useState(false)
-  const [transactions, setTransactions] = useState(null)
+  const [transactions, setTransactions] = useState<any[]>([])
+  const [selectedSeller, setSellectedSeller] = useState(allSellers)
+
+  const filterBySeller = (): any[] => {
+    if (selectedSeller === allSellers) return transactions
+    return transactions.filter((t) => t.sellerName === selectedSeller)
+  }
+  const filteredSellers = filterBySeller()
+
+  const changeSelectedSeller = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
+    setSellectedSeller(e.target.value)
+  }
+
+  const getTotalBalance = (): number => {
+    return filteredSellers.reduce(
+      (sum, t) => sum + (t.typeId === expenseTypeId ? -t.value : t.value),
+      0
+    )
+  }
+
+  const getTotalIncome = (): number => {
+    return filteredSellers.reduce(
+      (sum, t) => sum + (t.typeId === expenseTypeId ? 0 : t.value),
+      0
+    )
+  }
+
+  const getTotalExpense = (): number => {
+    return filteredSellers.reduce(
+      (sum, t) => sum + (t.typeId === expenseTypeId ? t.value : 0),
+      0
+    )
+  }
+
+  const getSellerOptions = (): string[] => {
+    const sellers = transactions.map((t) => get(t, "sellerName"))
+    return [allSellers, ...new Set(sellers)]
+  }
 
   const handleUploadModal = (): void => {
     setIsUploadModalOpen(!isUploadModalOpen)
@@ -58,17 +78,31 @@ function Transactions(): ReactElement {
     <PageLayout
       title="Transactions"
       actionElements={
-        <Button secondary onClick={handleUploadModal}>
-          <TbUpload size={18} />
-          Upload File
-        </Button>
+        <>
+          <Button secondary onClick={handleUploadModal}>
+            <TbUpload size={18} />
+            Upload File
+          </Button>
+          {transactions.length > 0 && (
+            <Select
+              options={getSellerOptions()}
+              onChange={(e) => changeSelectedSeller(e)}
+            />
+          )}
+        </>
+      }
+      rightElement={
+        <Balance
+          value={getTotalBalance()}
+          income={getTotalIncome()}
+          expense={getTotalExpense()}
+        />
       }
     >
       <Table
         emptyTitle="No transactions yet"
         emptySubtitle="Upload transactions files to view them here."
-        rows={transactions}
-        columns={transactionColumns}
+        rows={filterBySeller()}
       />
       <UploadModal isOpen={isUploadModalOpen} onClose={handleUploadModal} />
     </PageLayout>
