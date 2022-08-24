67373540ee1e19a61a1c8150b2440c7b1828ec72
isabela <isabelavdmatos@gmail.com>
2022-08-24 02:41:12 -0300

    dockerize the client

--- server/Program.cs
+++ server/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMvc();
 builder.Services.AddDbContext<ApplicationContext>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
+builder.WebHost.UseUrls("http://*:8080");
 
 var app = builder.Build();
 
@@ -17,11 +18,11 @@ app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "Cube v1");
-    options.RoutePrefix = string.Empty;
+    options.RoutePrefix = "swagger";
 });
-
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
