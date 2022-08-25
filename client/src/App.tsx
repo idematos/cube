a98d4bf172868145c7cd12646a5d10d35db99c41
isabela <isabelavdmatos@gmail.com>
2022-08-25 00:48:42 -0300

    remove template files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,27 @@
-import "./App.css"
 import { ReactElement } from "react"
 
 import { Navigate, Route, Routes } from "react-router-dom"
+import styled from "styled-components"
 
 import GlobalStyle from "./global-styles"
 import Login from "./views/login"
 import Transactions from "./views/transactions"
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
 function App(): ReactElement {
   return (
-    <div className="App">
+    <Container>
       <GlobalStyle />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/transactions" element={<Transactions />} />
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
-    </div>
+    </Container>
   )
 }
 
